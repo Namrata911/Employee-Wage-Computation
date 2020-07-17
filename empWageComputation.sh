dceb9a5fa9f2e8a582ac03104348f1dfba300e2d
Namrata <911npm@gmail.com>
2020-07-17 15:57:42 +0530

    1. Used switch case to calculate empwage. 2. Added new variable for working hours to calculate daily wage

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -6,8 +6,23 @@ then
 else
 	echo "employee is present"
 fi
+
 wagePerHr=20
 fulldayHour=8
 partTimeHour=4
-dailywage=$(( $wagePerHr*$fulldayHour ))
+workingHrs=0
+empType=$(( RANDOM%2 )) #0 for full time, 1 for part time
+
+case $empType in
+0)
+	workingHrs=$fulldayHour
+	echo "Full Time employee"
+;;
+1)
+	workingHrs=$partTimeHour
+	echo "Part Time employee"
+;;
+esac
+
+dailywage=$(( $wagePerHr*$workingHrs ))
 echo "Daily wage $dailywage"
